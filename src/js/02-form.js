4994883abc387a3a03a9f51c7b23781f621a30fc
Mesut Güzel <mesut.guzel@outlook.com.tr>
2025-06-07 17:18:21 +0300

    ödev dosyaları düzenlendi

---
+++ src/js/02-form.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('.feedback-form');
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const savedData = localStorage.getItem('feedback-form-state');
+if (savedData) {
+  formData = JSON.parse(savedData);
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (
+    !form.elements.email.value.trim() ||
+    !form.elements.message.value.trim()
+  ) {
+    alert('Please fill in all fields.');
+    return;
+  }
+
+  console.log(formData);
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+  formData = { email: '', message: '' };
+});
